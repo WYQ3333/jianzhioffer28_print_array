c01a3f553799692ff387a922ab741f4c93c1a805
浅忆梦微凉 <3538922590@qq.com>
2019-02-07 20:09:17 +0800

    返回一个数组

--- 剑指offer28_顺序打印矩阵/test.cpp
+++ 剑指offer28_顺序打印矩阵/test.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-class Solution {
+class Solution1 {
 public:
 	void printMatrix(vector<vector<int> > matrix) {
 		int count = 0;
@@ -42,11 +42,40 @@ public:
 	}
 };
 
+class Solution {
+public:
+	vector<int> printMatrix(vector<vector<int>> matrix) {
+		int row = matrix.size();
+		int col = matrix[0].size();
+		vector<int> result;
+		if (row == 0 || col == 0)
+			return result;
+		int left = 0, right = col - 1, top = 0, btm = row - 1;
+		while (left <= right&&top <= btm)
+		{
+			for (int i = left; i <= right; i++)
+				result.push_back(matrix[top][i]);
+			if (top<btm)
+			for (int i = top + 1; i <= btm; i++)
+				result.push_back(matrix[i][right]);
+			if (top<btm&&left<right)
+			for (int i = right - 1; i >= left; i--)
+				result.push_back(matrix[btm][i]);
+			if (top + 1<btm&&left<right)
+			for (int i = btm - 1; i >= top + 1; i--)
+				result.push_back(matrix[i][left]);
+			left++; right--; top++; btm--;
+		}
+		return result;
+	}
+};
+
 void TestFunc(){
 	vector<vector<int>> array = { {1,2,3,4}
 	, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
 	Solution s;
 	s.printMatrix(array);
+
 }
 
 int main(){
