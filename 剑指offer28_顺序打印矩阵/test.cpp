7397c6662ecb0a4fc34f0e950b7596aa49c103af
浅忆梦微凉 <3538922590@qq.com>
2019-02-07 19:22:09 +0800

    编译通过

---
+++ 剑指offer28_顺序打印矩阵/test.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+	void printMatrix(vector<vector<int> > matrix) {
+		int count = 0;
+		int row1 = 0;//0行
+		int col1 = 0;//0列
+		int row2 = matrix.size();//matrix.size()行
+		int col2 = matrix[0].size();//matrix[0].size()列
+		int i = 0;
+		int j = 0;
+		while (count < (col2*row2)){
+			for (i = col1; i < col2; ++i){
+				cout << matrix[i][j] << " ";
+			}
+			row1++;
+			i = col2 - 1;
+			for (j = row1; j < row2; ++j){
+				cout << matrix[i][j] << " ";
+			}
+			col2--;
+			j = row2 - 1;
+			for (i; i>col1; --i){
+				cout << matrix[i][j] << " ";
+			}
+			row2--;
+			for (j; j > row1; --j){
+				cout << matrix[i][j] << " ";
+			}
+		}
+	}
+};
+
+void TestFunc(){
+
+}
+
+int main(){
+	TestFunc();
+	system("pause");
+	return 0;
+}
